1f4887a69853855f3db122904300563ac86a38d9
Adigotlib <adi.gotlib@post.runi.ac.il>
2024-12-03 17:26:18 +0200

    trying)

--- Primes.java
+++ Primes.java
@@ -3,17 +3,20 @@ import java.util.Arrays;
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        boolean [] isprime = new boolean[n];
+        boolean [] isprime = new boolean[n+1];
         Arrays.fill(isprime, true);
         int p = 2;
         isprime [0] = false;
-        isprime [1] = false;
+        if ((n+1)> 1)
+        {
+            isprime [1] = false;
+        }
         // check where primes
-        while ( p * p < n)
+        while ( p * p < (n+1))
         {
             if (isprime[p])
             {
-                for (int i = p*p; i < n; i +=p)
+                for (int i = p*p; i < n+1; i +=p)
                 {
                     isprime[i] = false; 
                 }
@@ -23,15 +26,16 @@ public class Primes {
         // print all primes and check how muh precent
         int count = 0;
         System.out.println("Prime numbers up to " + n +":");
-        for (int z = 0; z < n; z++)
+        for (int z = 0; z < n+1; z++)
         {
-            if (isprime [z] == true)
+            if (isprime [z])
             {
                 System.out.println(z);
                 count ++;
             }
         }
-        int precent = (int)((100.0 * count) / n);
+
+        int precent = (int)((100.0 * count) / (n));
         System.out.println("There are " + count + " primes between 2 and " + n + " (" + precent + "% are primes)");
 
       
